6748a73a8063b1cc87c9028b88d1a3540a5b6724
MadAvenger <afoninmihael@gmail.com>
2019-05-30 14:38:46 +0300

    Убрал запятую. Склеил массив в строку.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 30},
-    {id: 3, title: 'Keyboard', price: 55},
-    {id: 4, title: 'Gamepad', price: 65},
-    {id: 5, title: 'Chair', price: 165},
+    { id: 1, title: 'Notebook', price: 2000 },
+    { id: 2, title: 'Mouse', price: 30 },
+    { id: 3, title: 'Keyboard', price: 55 },
+    { id: 4, title: 'Gamepad', price: 65 },
+    { id: 5, title: 'Chair', price: 165 },
 ];
+
+
 
 const renderProduct = (title, price) => {
     return `<div class="product-item">
@@ -15,8 +17,11 @@ const renderProduct = (title, price) => {
 };
 
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
+    let productsList = list.map(item => renderProduct(item.title, item.price));
+    productsList = productsList.join(' ');
     document.querySelector('.products').innerHTML = productsList;
+
 };
+
 
 renderPage(products);

1ae52e74f2678f3a89e0a2d4415379b9e26fd697
MadAvenger <afoninmihael@gmail.com>
2019-06-03 17:50:30 +0300

    Заменил свой проект, на проект с урока.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,53 @@
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Mouse', price: 30 },
-    { id: 3, title: 'Keyboard', price: 55 },
-    { id: 4, title: 'Gamepad', price: 65 },
-    { id: 5, title: 'Chair', price: 165 },
-    { id: 6, title: 'Table', price: 200 },
-    { id: 7, title: 'Sound system', price: 100 },
-    { id: 8, title: 'Motherboard', price: 120 },
-    { id: 9, title: 'RAM', price: 80 },
-    { id: 10, title: 'HDD', price: 100 }
-];
+class ProductsList {
+    constructor(container = '.products') {
+        this.data = [];
+        this.container = container;
+        this.productsAll = [];
+        this.init();
+    }
+    init() {
+        this._fetchProducts();
+        this._render();
+    }
+    _fetchProducts() {
+        this.data = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 30 },
+            { id: 3, title: 'Keyboard', price: 55 },
+            { id: 4, title: 'Gamepad', price: 65 },
+            { id: 5, title: 'Chair', price: 120, template: 1 },
+        ];
+    }
+    _render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.data) {
+            const prod = new ProductItem(product);
+            this.productsAll.push(prod);
+            block.insertAdjacentHTML('beforeend', prod.render());
+        }
+    }
+}
 
 
+class ProductItem {
+    constructor(product, img = `https://placehold.it/200x150`) {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img
+    }
 
-const renderProduct = (title, price) => {
-    return `<div class="product-item col-sm-12 col-md-3 col-lg-2">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
-};
-
-const renderPage = list => {
-    let productsList = list.map(item => renderProduct(item.title, item.price));
-    productsList = productsList.join(' ');
-    document.querySelector('.products').innerHTML = productsList;
-
-};
+    render() {
+        return `<div class="product-item">
+                  <img src="${this.img}" alt="${this.title}">
+                  <div class="desc">
+                      <h3>${this.title}</h3>
+                      <p>${this.price}</p>
+                      <button class="buy-btn">Купить</button>
+                  </div>
+              </div>`
+    }
+}
 
 
-renderPage(products);
+const products = new ProductsList();
